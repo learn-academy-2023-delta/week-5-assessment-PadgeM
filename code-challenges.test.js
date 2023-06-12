2a0456242ee788b88e188eeea883d6437bfbb5be
PadgeM <padge.mattaliano@gmail.com>
2023-06-12 09:26:10 -0700

    adds answers

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,29 +14,92 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+describe('encodeString', () => {
+  it('takes in a string and returns a string with a coded message', () => {
+    const secretCodeWord1 = "Lackadaisical"
+    expect(encodeString(secretCodeWord1)).toBe("L4ck4d41s1c4l")
+
+    const secretCodeWord2 = "Gobbledygook"
+    expect(encodeString(secretCodeWord2)).toBe("G0bbl3dyg00k")
+
+    const secretCodeWord3 = "Eccentric"
+    expect(encodeString(secretCodeWord3)).toBe("3cc3ntr1c")
+  })
+})
 
 // b) Create the function that makes the test pass.
 
+const encodeString = (message) => {
+  const encodingMap = {
+    a: '4',
+    e: '3',
+    i: '1',
+    o: '0'
+  }
+
+  let encodedMessage = ''
+
+  for (let i = 0; i < message.length; i++) {
+    const char = message[i].toLowerCase()
+
+    if (encodingMap.hasOwnProperty(char)) {
+      encodedMessage += encodingMap[char]
+    } else {
+      encodedMessage += message[i]
+    }
+  }
+
+  return encodedMessage
+}
+
+
 // Pseudo code:
+// I start by defining a function called encodeString that accepts a message as input.
+// Inside the function, I create an object called encodingMap. This object contains a mapping of certain characters (specifically 'a', 'e', 'i', and 'o') to their encoded equivalents ('4', '3', '1', and '0' respectively).
+// I initialize an empty string variable called encodedMessage. This variable will store the encoded version of the input message.
+// I loop through each character in the input message. Starting from the first character and going up to the last character:
+  // a. I convert the current character to lowercase and store it in a variable called char.
+  // b. I check if the encodingMap object contains the current character (char). If it does, I append the corresponding encoded value from the encodingMap to the encodedMessage string.
+  // c. If the current character is not in the encodingMap, I simply append the original character to the encodedMessage string.
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe('filterWordsByLetter', () => {
+  const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+  
+  it('returns an array of words containing the letter "a"', () => {
+    const filterLetter = "a"
+    const expectedOutput = ["Mango", "Apricot", "Peach"]
+    const filteredWords = filterWordsByLetter(fruitArray, filterLetter)
+    expect(filteredWords).toEqual(expectedOutput)
+  })
 
-const filterLetterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const filterLetterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+  it('returns an array of words containing the letter "e"', () => {
+    const filterLetter = "e"
+    const expectedOutput = ["Cherry", "Blueberry", "Peach"]
+    const filteredWords = filterWordsByLetter(fruitArray, filterLetter)
+    expect(filteredWords).toEqual(expectedOutput)
+  })
+})
 
 // b) Create the function that makes the test pass.
+
+const filterWordsByLetter = (words, letter) => {
+  return words.filter(word => word.toLowerCase().includes(letter.toLowerCase()))
+}
+
+// Psuedo code:
+// Okay, so I have a function called filterWordsByLetter. It takes in two inputs: words and letter. words is an array of words, and letter is a specific letter that I want to filter by.
+// Inside the function, I're using the filter method on the words array. This method helps us create a new array based on certain criteria.
+// The filter method takes a callback function as an argument. In this case, the callback function is defined using arrow function syntax and has one parameter named word. This word represents each individual word in the words array.
+// Now, within the callback function, I do a couple of things to each word before deciding whether to include it in the filtered array or not.
+// First, I convert the word to lowercase using the toLowerCase method. This is done to make the comparison case-insensitive. I want to match the letter regardless of whether it's uppercase or lowercase.
+// After converting the word to lowercase, I use the includes method on the word to check if it contains the letter. The includes method returns true if the letter is found in the word, and false otherwise.
+// If the includes method returns true, it means the word includes the letter, so I include that word in the filtered array.
+// The filter method continues this process for each word in the words array, building a new array that only contains the words meeting our criteria.
+// Finally, the function returns the filtered array, which consists of the words from the original array that include the specific letter, regardless of their case.
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
@@ -44,15 +107,57 @@ const filterLetterE = "e"
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+describe('isFullHouse', () => {
+  it('should return true for a full house hand', () => {
+    const hand1 = [5, 5, 5, 3, 3]
+    const hand4 = [7, 2, 7, 2, 7]
+    expect(isFullHouse(hand1)).toBe(true)
+    expect(isFullHouse(hand4)).toBe(true)
+  })
+
+  it('should return false for a non-full house hand', () => {
+    const hand2 = [5, 5, 3, 3, 4]
+    const hand3 = [5, 5, 5, 5, 4]
+    expect(isFullHouse(hand2)).toBe(false)
+    expect(isFullHouse(hand3)).toBe(false)
+  })
+})
+
 
 // b) Create the function that makes the test pass.
 
+const isFullHouse = (hand) => {
+  // Count the occurrences of each number in the hand
+  const counts = {}
+  for (let i = 0; i < hand.length; i++) {
+    const num = hand[i]
+    counts[num] = counts[num] ? counts[num] + 1 : 1
+  }
+
+  // Check if there is exactly one pair and one three of a kind
+  let hasPair = false
+  let hasThreeOfAKind = false
+  for (const num in counts) {
+    if (counts[num] === 2) {
+      hasPair = true;
+    } else if (counts[num] === 3) {
+      hasThreeOfAKind = true
+    }
+  }
+
+  return hasPair && hasThreeOfAKind
+}
+
+
 // Pseudo code:
+// I need to declare a function called isFullHouse. The function uses arrow function syntax and takes one parameter named hand. This parameter represents an array of numbers that represents a hand of cards.
+// Inside the function, I create an empty object called counts. This object will be used to count the occurrences of each number in the hand array.
+// I then enter a for loop that iterates over each element in the hand array. The loop variable i represents the current index, and num represents the number at that index.
+// Within the loop, I update the counts object. If counts[num] already exists, it means I have encountered the number before, so I increment its count by 1. If it doesn't exist yet, I initialize it with a count of 1.
+// After the loop finishes, I have a counts object that contains the count of occurrences for each number in the hand array.
+// Next, I declare two variables, hasPair and hasThreeOfAKind, and initialize them as false. These variables will help us keep track of whether I have found a pair and a three of a kind in the hand.
+// I then enter another loop, this time using a for...in loop to iterate over the properties of the counts object. In each iteration, the loop variable num represents the current number.
+// Within the loop, I check if the count of the current number (counts[num]) is equal to 2. If it is, it means I have found a pair, so I set the hasPair variable to true.
+// If the count is not 2, I check if it is equal to 3. If it is, it means I have found a three of a kind, so I set the hasThreeOfAKind variable to true.
+// After iterating through all the numbers in the counts object, I have determined if I have a pair and a three of a kind in the hand.
+// Finally, I return the result of the logical expression hasPair && hasThreeOfAKind. This expression will be true if both hasPair and hasThreeOfAKind are true, indicating that I have a full house. Otherwise, it will be false.
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,60 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a Ruby class?
 
-Your answer:
+Your answer: 
+The super keyword lets us control everything within a certain class, I believe. 
 
 Researched answer:
+In Ruby, the super keyword is a command that lets us access and use the powers of a superclass from within a subclass. It's like having a secret button that says, "Hey, do everything the superclass does, and then I'll add my own twist!" When we call super inside a method of a subclass, it searches up the family tree to find a method with the same name in the superclass. Once it finds it, it activates the superclass's method, allowing us to reuse its existing code. This way, we can build upon the foundation laid by the superclass and customize it to fit our specific needs in the subclass.
 
 2. What is a gem?
 
-Your answer:
+Your answer: 
+A gem is special code that is like a shortcut. We install it to do certain things. Like testing Ruby.
 
 Researched answer:
+In Ruby, a "gem" is a special package that contains ready-to-use code created by other developers. It's kind of like a treasure chest full of useful stuff that we can add to our own Ruby projects. These gems can do all sorts of things, from helping us with common tasks to providing entire frameworks or applications. We can easily install and manage gems using RubyGems, which is like a manager for these packages. Gems have their own names, versions, and information that tells us what they do and how to use them. They save us time and effort by letting us use code that others have already written and shared with the community. It's like having a shortcut to building cool stuff with Ruby!
 
 3. What is a relational database? Are there other kinds of databases?
 
 Your answer:
+Ah damn, I can't remember this one. I really hate having to answer questions with this answer. I do have memory problems and I am trying to study as much as I can to overcome them. I just remember that databases are like storage for data. But I honestly can't remember anything else. 
 
 Researched answer:
+A relational database is like a structured way to store and organize data. It's all about tables, with rows and columns, and the cool thing is that we can define relationships between the tables. We use keys to make these connections. A primary key helps us identify each row uniquely in a table, and a foreign key lets us link one table to another. This way, we can find and work with data efficiently. But there are also other types of databases! Like NoSQL databases, which are great for handling unstructured or big data, and they have different flavors like document databases and key-value stores. There are also object-oriented databases that work well with object-oriented programming languages, hierarchical databases that use a tree-like structure, and network databases that allow for more complex relationships. So, depending on what we need and how our data is structured, we choose the type of database that suits our needs.
 
 4. What are primary keys? Why purpose does a primary key serve?
 
 Your answer:
+A primary key is a unique identifier within a database table that serves as a way to uniquely identify each record. It ensures data integrity, facilitates efficient data retrieval, and enables relationships between tables in a relational database. 
 
 Researched answer:
+Primary keys are important in a database. They are special columns in a table that make sure each row has a unique identifier. You can think of them like a fingerprint for each record. They serve several purposes. First, they guarantee that no two rows have the same key value, so we don't end up with duplicates or conflicting data. Second, they help with data integrity, making sure our data is accurate and reliable. Third, primary keys can be used to create relationships between tables, which is like connecting related data together. This makes it easier to work with data across different tables. Lastly, primary keys are indexed by the database system, so we can find and retrieve specific records faster. So, primary keys are like the superheroes of a database, keeping our data organized, unique, and easy to access!
 
 5. What are the five HTTP verbs? What is each verb's corresponding CRUD action?
 
 Your answer:
 
 Researched answer:
+The five HTTP verbs are GET, POST, PUT, DELETE, and PATCH. The corresponding CRUD actions are: GET retrieves data, POST creates new data, PUT updates existing data, DELETE removes data, and PATCH partially updates existing data.
+
+The five HTTP verbs, which also represent CRUD actions, are pretty important in web development. GET is for reading or retrieving data from a server, like when we want to see information on a website. POST is for creating new data, like when we submit a form and it adds something to the server. PUT is for updating or replacing existing data, which is useful when we need to change something completely. PATCH is for making partial updates to existing data, like when we want to modify just a few fields. Lastly, DELETE is for removing or deleting data from the server, like when we want to get rid of something for good. So, these HTTP verbs help us perform different actions and interact with web servers in a structured way.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
 1. Model validations:
+Model validations are rules or checks that are applied to the data within a model in order to ensure its correctness and integrity. In the context of web development, specifically with frameworks like Ruby on Rails, model validations are used to validate the data before it is saved to a database. These validations can enforce constraints on attributes, such as ensuring they are present, unique, within a specific range, or meet certain format requirements. By implementing model validations, developers can maintain data consistency and prevent invalid or inconsistent data from being stored.
 
 2. RESTful routes:
+RESTful routes, also known as RESTful APIs or RESTful endpoints, are a set of standardized conventions for defining the routes and actions in a web application's API (Application Programming Interface). REST, which stands for Representational State Transfer, is an architectural style that emphasizes a stateless and resource-based approach to web services. RESTful routes define the operations that can be performed on resources, such as creating, reading, updating, and deleting (CRUD) data. These routes follow a consistent pattern and are often named after the resources they manipulate, making it easier for developers to design, understand, and interact with APIs in a predictable manner.
 
 3. ERB:
+ERB stands for Embedded Ruby. It is a templating language used in Ruby-based web frameworks like Ruby on Rails. ERB allows developers to embed Ruby code within HTML or other markup files. With ERB, you can dynamically generate content by including Ruby expressions, loops, conditionals, and variable interpolation directly in the template files. During the rendering process, the embedded Ruby code is executed, and its results are combined with the surrounding markup to generate the final output. ERB provides a powerful way to generate dynamic web pages and separate logic from presentation.
 
 4. Params:
+In web development, "params" (short for parameters) refer to the data or information sent from a client to a server as part of an HTTP request. Params typically consist of key-value pairs and can include data from various sources, such as form inputs, query strings, or route parameters. They allow the client to send specific information to the server, which can then be used to perform operations or make decisions. In server-side programming, developers can access and process these params to retrieve user input, pass data to database queries, or perform other actions based on the received parameters.
 
 5. API:
+API stands for Application Programming Interface. It is a set of rules, protocols, and tools that allows different software applications to communicate and interact with each other. APIs define the methods and data formats that applications can use to request or exchange information. They act as intermediaries, enabling different systems or components to work together by providing a standardized way of accessing functionality or data. APIs are widely used in web development to integrate services, retrieve data from external sources, or expose functionality for other developers to use. They play a vital role in enabling interoperability and building modular, extensible software systems.
